98fdd76b356f65480b4ea9a173c6c019fdec3961
geunhong Lim <seelime11@gmail.com>
2022-06-20 14:44:55 +0900

    feat: GlobalStyle #6

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import GlobalStyle from "./styles/globalStyles";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" />
       </Routes>
+      <GlobalStyle />
     </BrowserRouter>
   );
 }

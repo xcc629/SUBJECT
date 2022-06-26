5ae34ee7e9a7e9771e79529551acadb9575c8807
geunhong Lim <seelime11@gmail.com>
2022-06-26 14:18:47 +0900

    docs: Readme.md 작성 및 다른 Md파일 포멧 수정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as React from "react";
+
 import AchievementSection from "../../components/organisms/AchievementSection";
 
 export default function Main() {
